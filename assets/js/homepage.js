f9c5a5b321d7cd601391598f985e76036613c038
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-07-08 12:48:13 -0500

    form is displayed and functional, but only shows response in the console.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,5 +9,22 @@ var getUserRepos = function(user) {
     });
 };
   
-  getUserRepos();
   
+  var userFormEl = document.querySelector("#user-form");
+  
+  var nameInputEl = document.querySelector("#username");
+
+  var formSubmitHandler = function(event) {
+      event.preventDefault();
+      //get value from input el
+      var username = nameInputEl.value.trim();
+        //check if theres a value in the usrname input
+      if(username){
+          getUserRepos(username);
+          nameInputEl.value = "";
+      }
+      else{
+          alert("please enter a github usrname");
+      }
+  };
+  userFormEl.addEventListener("submit", formSubmitHandler);
